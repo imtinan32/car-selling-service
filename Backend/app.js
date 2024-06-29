367db939a0f3e47e370392fba5f0fdea64880944
Rao Imtinan <2019cs32@stu.mnsuet.edu.pk>
2024-06-30 01:01:20 +0500

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,34 +2,41 @@ const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const cors = require('cors');
-const preData = require('./data/predata')
+const preData = require('./data/predata');
 const loginRouter = require('./routers/login.router'); 
 const vehicleRouter = require('./routers/vehicle.router'); 
 
 const app = express();
 const port = 3000;
 
-mongoose.connect("mongodb://127.0.0.1:27017/CarList", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Database Connected');
-  })
-  .catch((err) => {
-    console.error('Error connecting to database', err);
-  });
 
-app.use(cors());
-app.use(express.json()); 
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true
+}));
+app.use(express.json());
 app.use(session({
   secret: 'your-secret-key',
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false } 
 }));
-preData
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/CarList')
+  .then(async () => {
+    console.log('Database Connected');
+    
+    await preData(); 
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to database', err);
+  });
+
+
 app.use('/login', loginRouter); 
-app.use('/vehicles', vehicleRouter); 
-
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.use('/vehicles', vehicleRouter);
